87472952f126bbcee452316a5c7ce3a643579004
Ron O'hara <acid1789@gmail.com>
2018-07-26 23:43:15 -0700

    Fixed padding for osc strings and blobs

--- UnitySharedLib/OSCWriter.cs
+++ UnitySharedLib/OSCWriter.cs
@@ -18,7 +18,7 @@ namespace UnitySharedLib
 			base.Write(strBytes, 0, value.Length);
 			base.Write((byte)0);
 			int writtenSize = value.Length + 1;
-			int padding = writtenSize % 4;
+			int padding = 4 - (writtenSize % 4);
 			if (padding != 0)
 				base.Write(new byte[padding], 0, padding);
 		}
@@ -27,7 +27,7 @@ namespace UnitySharedLib
 		{
 			base.Write((int)blob.Length);
 			base.Write(blob, 0, blob.Length);			
-			int padding = blob.Length % 4;
+			int padding = 4 - (blob.Length % 4);
 			if( padding != 0 )
 				base.Write(new byte[padding], 0, padding);
 		}
